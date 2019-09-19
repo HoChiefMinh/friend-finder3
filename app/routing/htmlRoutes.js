dd81c3adfa101054a512fb5e43ca3d9c49df35ef
Minh Pham <mqpham848@gmail.com>
2019-09-19 19:04:40 -0400

    fix package.json

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,14 +4,13 @@ var path = require("path");
 // ROUTING
 module.exports = function(app) {
 
-    // Catch-all route to homepage
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-    
     // HTML GET Requests
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     
+    // Catch-all route to homepage
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };
